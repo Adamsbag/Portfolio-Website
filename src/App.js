6804ca6c4b5b6a30583c13a9551a945b5c8ec4be
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2022-09-09 11:44:59 -0400

    Custom React

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my Portfolio Website!!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/adamsbag"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          <h1>Hello from V2</h1>
+          Checkout my GitHub repositories
         </a>
       </header>
     </div>
