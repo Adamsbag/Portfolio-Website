c6ea7f0329ffc6dcd713e6ff93826b3c8a389233
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2022-09-10 13:16:46 -0400

    New update to job role

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Adama Bagayoko</h1>
-        <h5 className="text-light">Cloud Engineer | AWS | Azure</h5>
+        <h5 className="text-light">DevOps | AWS</h5>
         <CTA />
         <HeaderSocial />
         <div className='me'>
--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -6,7 +6,7 @@ import {BsTwitter} from 'react-icons/bs'
 const HeaderSocial = () => {
   return (
     <div className='header__social'>
-        <a href='https://linkedin.com/in/adama-bagayoko-680bb9163/' target='_blank'><BsLinkedin/></a>
+        <a href='https://linkedin.com/adama-bagayoko/devops/' target='_blank'><BsLinkedin/></a>
         <a href='https://github.com/Adamsbag' target='_blank'><BsGithub/></a>
         <a href='https://twitter.com/AdamsBag' target='_blank'><BsTwitter/></a>
 
